180c1d9d2ddbb4b2e1d72362eeaaf1d19b912e89
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-04-04 01:56:33 +0200

    Fixed material ui icon import.

--- src/components/header_and_footer/header.jsx
+++ src/components/header_and_footer/header.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/Toolbar';
-import MenuIcon from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 class Header extends Component {
     render() {
