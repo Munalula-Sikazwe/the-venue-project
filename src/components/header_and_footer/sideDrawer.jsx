c419cc2cc5e9ae491cefa0144dc9541fee3fdbd7
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-04-04 05:16:15 +0200

    Created the SideDrawer scaffolding.

--- src/components/header_and_footer/sideDrawer.jsx
+++ src/components/header_and_footer/sideDrawer.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 
 const SideDrawer = () => {
     return (
         <div>
-
+<Drawer>
+    anchor={"right"}
+    open={}
+    onClose={}
+</Drawer>
         </div>
     );
 };
