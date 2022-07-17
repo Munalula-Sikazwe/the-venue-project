a3122f84e580609d25eb77267c50dc4f72d5e2d2
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-07-17 22:37:58 +0200

    Changed background Image css style

--- src/components/featured/Carousel.jsx
+++ src/components/featured/Carousel.jsx
@@ -1,38 +1,33 @@
-import React from 'react';
-import Slider from 'react-slick';
-import slide_one from '../../resources/images/slide_one.jpg';
-import slide_two from '../../resources/images/slide_two.jpg';
-import slide_three from '../../resources/images/slide_three.jpg';
- export default function 
-() {
-    let settings = {
-        dots:false,
-        infinte:true,
-        autoPlay:true,
-        speed:500
-          }
+import React from "react";
+import Slider from "react-slick";
+import slide_one from "../../resources/images/slide_one.jpg";
+import slide_two from "../../resources/images/slide_two.jpg";
+import slide_three from "../../resources/images/slide_three.jpg";
+export default function () {
+  let settings = {
+    dots: false,
+    infinte: true,
+    autoPlay: true,
+    speed: 500,
+  };
   return (
-    <div 
-    className={"carrousel_wrapper"
-    }
-    style={{
-        height:`${window.innerHeight}px`,
-        overflow:"hidden"
-    }}
+    <div
+      className={"carrousel_wrapper"}
+      style={{
+        height: `${window.innerHeight}px`,
+        overflow: "hidden",
+      }}
     >
-<Slider
-{...settings}
->
-<div>
-    <img src={slide_one}/>
-</div>
-<div>
-    <img src={slide_two}/>
-</div>
-<div>
-    <img src={slide_three}/>
-</div>
-</Slider>
+      <Slider {...settings}>
+        <div>
+          <div
+            style={{
+              backgroundImage: `url(${slide_one})`,
+              height: `${window.innerHeight}px`,
+            }}
+          ></div>
+        </div>
+      </Slider>
     </div>
-  )
+  );
 }
