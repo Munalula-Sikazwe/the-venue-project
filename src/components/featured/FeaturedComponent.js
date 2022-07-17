0e7a5a67c8b8aeb85f90369a0c88422f3964beef
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-07-17 18:34:05 +0200

    Created featured component Scaffolding.

---
+++ src/components/featured/FeaturedComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div style={{position:"relative"}}>
+<div className={"artists_name"}>
+    <div
+    className={"wrapper"}
+    >
+Ariana Grande
+    </div>
+
+</div>
+    </div>
+  )
+}
