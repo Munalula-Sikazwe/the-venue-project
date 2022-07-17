30b2d28b9ebe337db8c904ff83e267ac181fbf2d
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-07-17 22:23:25 +0200

    Completed the Carousel component.

--- src/components/featured/FeaturedComponent.jsx
+++ src/components/featured/FeaturedComponent.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Carousel from "./Carousel";
+
 export default function () {
   return (
     <div style={{ position: "relative" }}>
+      <Carousel/>
       <div className="artist_name">
         <div className="wrapper">Ariana Grande</div>
       </div>
