a880163e1f802afb5d7e438f2006a1908fdefa2e
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-07-18 23:54:11 +0200

    Completed the Count down Design

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from './components/header_and_footer/header';
 import FeaturedComponent from './components/featured/FeaturedComponent';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import TimeUntil from './components/featured/TimeUntil';
 const App = ()=> {
   return (
     <div className="App" style={{height:"1500px",background:"cornflowerblue"}}>
      <Header />
      <FeaturedComponent/>
+     <TimeUntil/>
     </div>
   );
 }
--- src/components/featured/TimeUntil.jsx
+++ src/components/featured/TimeUntil.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export default function TimeUntil() {
   return (
-    <div>TimeUntil</div>
-  )
+    <div className={"countdown_wrapper"}>
+      <div className={"countdown_top"}>Event Starts In</div>
+      <div className={"countdown_bottom"}>
+        <div className={"countdown_item"}>
+          <div className={"countdown_time"}>1</div>
+          <div className={"countdown_tag"}>Days</div>
+        </div>
+        <div className={"countdown_item"}>
+          <div className={"countdown_time"}>2</div>
+          <div className={"countdown_tag"}>HS</div>
+        </div>
+        <div className={"countdown_item"}>
+          <div className={"countdown_time"}>3</div>
+          <div className={"countdown_tag"}>Min</div>
+        </div>
+        <div className={"countdown_item"}>
+          <div className={"countdown_time"}>56</div>
+          <div className={"countdown_tag"}>Sec</div>
+        </div>
+      </div>
+    </div>
+  );
 }
