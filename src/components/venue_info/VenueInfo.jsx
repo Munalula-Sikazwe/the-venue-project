7a871df8506016d3da9dcfeae4dcdc0e3c9885e2
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-07-21 00:51:09 +0200

    Imported Icons

--- src/components/venue_info/VenueInfo.jsx
+++ src/components/venue_info/VenueInfo.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import CalenderIcon from '../../resources/images/calendar.png';
+import LocationIcon from '../../resources/images/location.png';
 export default function VenueInfo() {
   return (
     <div>VenueInfo</div>
