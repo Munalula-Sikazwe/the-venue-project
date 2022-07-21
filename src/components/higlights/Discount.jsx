16096395eadcb18c23bf4a4d59512fdadd358aec
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-07-22 00:43:48 +0200

    Completed the discountPercent function.

--- src/components/higlights/Discount.jsx
+++ src/components/higlights/Discount.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
+import { Fade, Slide } from "react-reveal";
 
 export default function Discount() {
-    const [discount, setDiscount] = useState(30); 
-  return (
-    <div
-    className={"center_wrapper"}
+  const [discountStart, setDiscountStart] = useState(0);
+  const [discountEnd, setDiscountEnd] = useState(30);
 
-    >
-        <div
-        className={"discount_wrapper"}
+    const discountPercent = () => {
+        if (discountStart < discountEnd) {
+            setDiscountStart(discountStart + 1);
+        }
+    }
+    
+  return (
+    <div className={"center_wrapper"}>
+      <div className={"discount_wrapper"}>
+        <Fade
+        onReveal={discountPercent}
         >
-            <div className={"discount_porcentage"}>
-<span>{discount}%</span>
-<span>OFF</span>
-            </div>
+          <div className={"discount_porcentage"}>
+            <span>{discountStart}%</span>
+            <span>OFF</span>
+          </div>
+        </Fade>
+        <Slide>
+        <div className={"discount_description"}>
+          <h3>Purchase tickets before 20th June</h3>
+          <p>Order now while stocks last</p>
         </div>
+        </Slide>
+       
+      </div>
     </div>
-  )
+  );
 }
