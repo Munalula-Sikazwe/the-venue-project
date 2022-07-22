c56bfa5956f0b98ca98a1604c7e3635a75551b0e
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-07-22 02:51:56 +0200

    Completed button scafolding

--- src/components/higlights/Discount.jsx
+++ src/components/higlights/Discount.jsx
@@ -1,35 +1,42 @@
 import React from "react";
-import { useState } from "react";
+import { useCallback } from "react";
+import { useState, useEffect } from "react";
 import { Fade, Slide } from "react-reveal";
 
 export default function Discount() {
   const [discountStart, setDiscountStart] = useState(0);
   const [discountEnd, setDiscountEnd] = useState(30);
+  useEffect(() => {
+    setTimeout(discountPercent, 30);
 
-    const discountPercent = () => {
-        if (discountStart < discountEnd) {
-            setDiscountStart(discountStart + 1);
-        }
-    }
     
+  }, [discountStart]);
+  const discountPercent = useCallback(() => {
+    if (discountStart < discountEnd) {
+      setDiscountStart(discountStart + 1);
+    }
+  }, [discountStart, discountEnd]);
+  
+console.log(discountStart)
   return (
     <div className={"center_wrapper"}>
       <div className={"discount_wrapper"}>
-        <Fade
-        onReveal={discountPercent}
-        >
+        <Fade onReveal={discountPercent}>
           <div className={"discount_porcentage"}>
             <span>{discountStart}%</span>
             <span>OFF</span>
           </div>
         </Fade>
         <Slide>
-        <div className={"discount_description"}>
-          <h3>Purchase tickets before 20th June</h3>
-          <p>Order now while stocks last</p>
-        </div>
+          <div className={"discount_description"}>
+            <h3>Purchase tickets before 20th June</h3>
+            <p>Order now while stocks last</p>
+            <div>
+            Button
+          </div>
+          </div>
+         
         </Slide>
-       
       </div>
     </div>
   );
---
+++ src/components/ui/MyButton.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Button } from '@material-ui/core';
+import Ticket_Icon from '../../assets/images/icons/ticket.png';
+export default function MyButton() {
+  return (
+    <Button 
+    href={'https://www.google.com'}
+    size={"small"}
+    style={{
+        backgroundColor: 'red',
+        color: 'white',
+    }}
+    variant="contained" 
+    color="primary"
+    >
+        <img
+        src={Ticket_Icon}
+        alt={"icon_image"}
+        className={"iconImage"}
+        />
+
+Purchase Tickets
+    </Button>
+  )
+}
